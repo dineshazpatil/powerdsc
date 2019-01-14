f9dd6ea7211360e29a7066055e309e2a3c625d47
dineshazpatil <42247517+dineshazpatil@users.noreply.github.com>
2019-01-14 16:07:57 +0530

    dsctesttask1

---
+++ dsctesttask1.ps1
@@ -0,0 +1,41 @@
+﻿$configdata= @{
+
+    AllNodes = @(
+    
+            @{
+                Nodename="Test1"
+                Features = @("Web","TelnetClient")
+            }
+    
+        )
+    
+
+
+}
+
+
+
+Configuration FirstConfiguration
+{ 
+
+  
+    Import-DscResource -ModuleName PSDesiredstateconfiguration
+    Node $Allnodes.Nodename
+    {
+    
+        WindowsFeatureSet $feature
+        
+        {
+            Name = $node.features
+            Ensure = "Present"
+        
+        }
+
+    
+    }
+}
+
+
+Firstconfiguration -OutputPath C:\Demo\FirstConfiguration -ConfigurationData $configdata
+
+
