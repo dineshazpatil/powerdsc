cee2c087abbfc5e2556c6daaa89f1990728030aa
dineshazpatil <42247517+dineshazpatil@users.noreply.github.com>
2019-02-22 20:08:43 +0530

    updated

---
+++ Powershell Workflow.ps1
@@ -0,0 +1,105 @@
+﻿# In sequence
+
+workflow test-sequence
+{
+    sequence 
+    {
+        Get-CimInstance win32_bios
+        Get-CimInstance win32_computerSystem
+    
+    }
+
+
+}
+
+test-sequence
+measure-command {1..100 | foreach{$test=test-sequence}}
+
+workflow test-parallel
+{
+    Parallel 
+    {
+        Get-CimInstance win32_bios
+        Get-CimInstance win32_computerSystem
+    
+    }
+
+
+}
+
+test-parallel
+measure-command {1..100 | foreach {$test2=test-parallel}}
+
+
+workflow Test-Foreachparallel
+{
+    $objs = @("win32_bios","win32_computersystem")
+
+    foreach -Parallel ($obj in $objs) #-throttlelimit
+    {
+        Get-CimInstance $obj
+    
+    }
+
+}
+Test-Foreachparallel
+
+
+
+Restrictions:
+
+Some Automatic Variables unavailable
+    
+    $args $Error and few more
+
+Some cmdlets you can not call
+
+*-alias *-psdrive *-variable debug-process Get-credential Read-host show-command show-eventlog
+
+Some PowerShell language features you can not see
+
+$Date variable is not allowed
+You cannot set assign enviornment variable 
+SubExpression Begin/Process/End 
+
+
+those not suported can be run 
+
+Inlinescript {write-host }
+
+
+#suspend-workflow 
+Workflow Test-Suspend
+{
+    $a = Get-Date
+    Suspend-Workflow
+    (Get-Date)- $a
+}
+Test-Suspend
+
+
+restart-computer in workflow
+
+workflow New-ComputerSetup
+{
+   ...
+   $cn =  Get-CimInstance -ClassName Win32_ComputerSystem
+   if ($cn.SystemType -like "64*") 
+   {
+       $NewCnName = $PSComputerName + "64"
+       Rename-Computer -ComputerName $PSComputerName -NewName $NewCnName
+   }
+   Restart-Computer -Wait
+   $disks = Get-Disk
+   ...
+} 
+
+$AtStartup = New-JobTrigger -AtStartup
+
+
+Register-ScheduledJob -Name ResumeWorkflow -Trigger $AtStartup -ScriptBlock {Import-Module PSWorkflow; Get-Job ComputerSetup -State Suspended | Resume-Job}
+
+New-ComputerSetup -JobName ComputerSetup
+Get-Job ComputerSetup
+
+Unregister-ScheduledJob -Name ComputerSetup
---
+++ net usage.ps1
@@ -0,0 +1,170 @@
+﻿#COM usage
+
+function invoke-standby
+{
+    add-type -AssemblyName System.windows.forms
+    [System.Windows.Forms.Application]::SetSuspendState(0,0,0) | out-null
+
+}
+
+invoke-standby
+
+function test-ipaddress($value)
+{
+    ($value -as [System.Net.IPAddress]) -ne $null -and ($value -as [Int] -eq $null)
+
+}
+test-ipaddress -value 10.1.1.1
+
+
+
+
+[System.Enum]::GetNames([System.DayOfWeek])
+
+[System.TimeZoneInfo]::Local
+
+[System.Media.SystemSounds]::Beep.Play()
+[System.Media.SystemSounds]::Asterisk.Play()
+[System.Media.SystemSounds]::Exclamation.Play()
+[System.Media.SystemSounds]::hand.Play()
+
+[Datetime]::DaysInMonth(2009,3)
+
+[datetime]::IsLeapYear(2009)
+
+DNS Lookup
+
+[System.Net.Dns]::GetHostByName("www.google.com")
+
+[Security.Principal.WindowsIdentity]::GetCurrent().User
+
+function test-admin 
+{
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $user = New-Object Security.Principal.windowsPrincipal $identity
+    $user.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+}
+
+
+test-admin
+
+
+[System.Math]::Sqrt(36)
+
+
+$DateTime = New-Object System.DateTime  -ArgumentList 2015, 10, 10
+
+[System.IO.File]::ReadAllText
+
+[System.IO.Compression]
+
+[system.console] | Get-Member -Static -MemberType method | Format-Table name,definition -AutoSize
+
+[System.Console]::Beep()
+
+[System.Console]::Clear()
+[console]::WindowHeight
+[console]::BackgroundColor
+[console]::ForegroundColor
+
+[console]::WindowHeight = 14
+
+[console]::ForegroundColor = “yellow”
+
+[console]::Title = "HI this is Dinesh"
+
+Add-Type -AssemblyName System.speech
+$talker = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+
+
+$talker.speak("hello there")
+
+$talker.GetInstalledVoices() | gm
+
+$talker.GetInstalledVoices().voiceinfo
+
+$talker.GetInstalledVoices().voiceinfo.name
+
+$talker.SelectVoice("Microsoft Zira Desktop")
+
+
+$talker.SetOutputToWaveFile(".\test.wav")
+$talker.Speak("Would you like to play a game?")
+$talker.Dispose()
+
+
+
+
+
+
+
+
+
+add-type -AssemblyName System.Drawing
+$mybrush = New-Object Drawing.SolidBrush green
+$mypen = new-object Drawing.Pen black
+$mypen | gm
+$mybrush | gm
+
+$mypen.color = "red"
+$mypen.width = 10
+
+
+
+
+
+
+
+$form = New-Object Windows.Forms.Form
+$formGraphics = $form.createGraphics()
+
+$form.add_paint({
+
+$formGraphics.DrawLine($mypen, 10,10,190,190)
+$formGraphics.FillEllipse($mybrush, 20,20,180,180)
+
+
+})
+
+
+$form.ShowDialog()
+
+$ButtonType = [System.Windows.MessageBoxButton]::OK
+$MessageIcon = [System.Windows.MessageBoxImage]::Warning
+$MessageBody = "Your Computer is going to shutdown in 10 Minutes after patch update"
+$MessageTitle = "Computer Restart"
+ 
+$Result =[System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
+  
+Write-Host "Your choice is $Result"
+
+
+$version = new-object -typename System.version -Argumentlist 1.2.3.4
+$version
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://docs.microsoft.com/en-us/powershell/scripting/samples/creating-a-custom-input-box?view=powershell-6
+
+http://techgenix.com/building-powershell-gui-part8/
+
+
+
+
+
+
+
